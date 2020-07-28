f3c4de483822cdc7a82db60d453a2b2561c3544d
Alan Ktquez <ktquez@gmail.com>
2020-07-28 15:03:25 -0300

    Add example in demo

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -2,56 +2,93 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Vue component that helps you to to trap focus in an element.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="activeTrap = !activeTrap">
+      {{ activeTrap ? 'Hide' : 'Show' }} links with focus loop
+    </button>
+    <FocusLoop v-if="activeTrap">
+      <ul class="links">
+        <li>
+          <a
+            href="https://vuejs.org"
+            target="_blank"
+            rel="noopener"
+          >Core Docs</a>
+        </li>
+        <li>
+          <a
+            href="https://forum.vuejs.org"
+            target="_blank"
+            rel="noopener"
+          >Forum</a>
+        </li>
+        <li>
+          <a
+            href="https://chat.vuejs.org"
+            target="_blank"
+            rel="noopener"
+          >Community Chat</a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/vuejs"
+            target="_blank"
+            rel="noopener"
+          >Twitter</a>
+        </li>
+        <li>
+          <a
+            href="https://news.vuejs.org"
+            target="_blank"
+            rel="noopener"
+          >News</a>
+        </li>
+        <li style="margin-top: 20px;">
+          <button @click="activeTrap = false">
+            Close
+          </button>
+        </li>
+      </ul>
+    </FocusLoop>
   </div>
 </template>
 
 <script>
+import { FocusLoop } from '../../vue-focus-loop'
+
 export default {
   name: 'HelloWorld',
+
+  components: {
+    FocusLoop
+  },
+
   props: {
     msg: String
-  }
+  },
+
+  data: () => ({
+    activeTrap: false
+  })
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+a {
+  color: #333;
 }
 ul {
   list-style-type: none;
-  padding: 0;
+  padding-left: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin-bottom: 10px;
 }
-a {
-  color: #42b983;
+.links {
+  background-color: #f2f2f2;
+  padding-top: 20px;
+  padding-bottom: 20px;
 }
 </style>
