31a5f60eabfa4218136cbffa662a70b28527d14b
Alan Ktquez <ktquez@gmail.com>
2021-01-15 15:49:24 -0300

    style: Outline style for demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -28,4 +28,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+[tabindex]:not([tabindex="-1"]), a, button, input, select, textarea {
+  position: relative;
+  z-index: 2;
+  outline-width: 3px !important;
+  outline-style: dotted !important;
+  outline-color: transparent !important;
+  outline-offset: 6px;
+  transition: outline-offset .1s ease;
+}
+
+*:focus {
+  outline-color: #333 !important;
+  outline-offset: 2px;
+}
+
+[tabindex="-1"] {
+  outline: 0 !important;
+}
 </style>
