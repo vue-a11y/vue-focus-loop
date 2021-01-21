2e1b05cf67303db9d51ba0564bb9bd0d9d25facc
Alan Ktquez <ktquez@gmail.com>
2021-01-21 16:44:08 -0300

    fix: Add type App

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { App } from 'vue'
 import FocusLoop from './FocusLoop.vue'
 
-export default function install (app: any): void {
+export default function install (app: App): void {
   app.component('FocusLoop', FocusLoop)
 }
 
